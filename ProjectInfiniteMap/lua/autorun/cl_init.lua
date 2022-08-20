70bef81048d19563ac1e65b132cfb700cf3a0479
UHmn <31524901+uhmn@users.noreply.github.com>
2022-08-19 20:08:23 -0700

    Added procedural terrain, removed pim_persistent

--- ProjectInfiniteMap/lua/autorun/cl_init.lua
+++ ProjectInfiniteMap/lua/autorun/cl_init.lua
@@ -1,4 +1,5 @@
 
+--AddCSLuaFile()
 
 if (!SERVER) then
 	print("SpaceTest Client Started")
@@ -32,7 +33,87 @@ if (!SERVER) then
 	net.Receive( "net_pim_dimensiontable", function( len, ply )
 		table.insert( pim_dimensiontable, net.ReadEntity() )
 	end )
+	
+	local function terrainupdate (plypos, vert, mu, location, center, gridsize)
+		local pl_x = plypos[1]
+		local pl_z = plypos[2]
+		local pl_y = plypos[3]
+		local x = pim_DimToPos(vert[1].pos, center, location)[1]
+		local z = pim_DimToPos(vert[1].pos, center, location)[2]
+		local xd = x%gridsize
+		local zd = z%gridsize
+		for i = 1, #vert, 1 do
+			local xa = vert[i].pos[1]
+			local za = vert[i].pos[2]
+			xa = xa - xd
+			za = za - zd
+			vert[i].pos = Vector(xa, za, ((pperlin(location[1] + (xa-center[1]), location[2] + (za-center[2]), 4)-location[3])+center[3]) )
+			--vert[i].pos = Vector(xa, za, ((0-location[3])+center[3]) )
+		end
+		return (vert)
+	end
+	
+	local function skyterrainupdate (plypos, vert, mu, location, skypos)
+		local pl_x = plypos[1]/mu
+		local pl_z = plypos[2]/mu
+		local pl_y = plypos[3]/mu
+		for i = 1, #vert, 1 do
+			local xa = vert[i].pos[1]
+			local za = vert[i].pos[2]
+			vert[i].pos = Vector(xa+pl_x, za+pl_z, ((pperlin((((xa+pl_x)-skypos[1])*64)+location[1], (((za+pl_z)-skypos[2])*64)+location[2], 2)-location[3])/mu)+7975)
+		end
+		return (vert)
+	end
+	
+	local function fillvert (power, vert1, vert2)
+		local vert = {}
+		local x1 = vert1[1]
+		local z1 = vert1[2]
+		local x2 = vert2[1]
+		local z2 = vert2[2]
+		local x2b = ( ( x2 - x1 ) / power ) + x1
+		local z2b = ( ( z2 - z1 ) / power ) + z1
+		local xb = math.abs(x1 - x2b)
+		local zb = math.abs(z1 - z2b)
+		local count = 1
+		for i = 1, power, 1
+		do
+			for e = 1, power, 1
+			do
+				local xb1 = x1 + (xb*(e))
+				local zb1 = z1 + (zb*(i))
+				local xb2 = x2b + (xb*(e))
+				local zb2 = z2b + (zb*(i))
+				vert[count*6-5] = { pos = Vector( xb2 , zb2 , 0) , u = 1, v = 1 }
+				vert[count*6-4] = { pos = Vector( xb2 , zb1 , 0) , u = 1, v = 0 }
+				vert[count*6-3] = { pos = Vector( xb1 , zb1 , 0) , u = 0, v = 0 }
+				vert[count*6-2] = { pos = Vector( xb1 , zb2 , 0) , u = 0, v = 1 }
+				vert[count*6-1] = { pos = Vector( xb2 , zb2 , 0) , u = 1, v = 1 }
+				vert[count*6  ] = { pos = Vector( xb1 , zb1 , 0) , u = 0, v = 0 }
+				count = count + 1
+			end
+		end
+		return (vert)
+	end
+	
 
+	pim_size = Vector( 7616, 7616, 7616 )
+	--verts = fillvert(12, 12, -8500, Vector(-7662, -7747, -15797), Vector(7570, 7484, -565), 1, 4, pim_location, pim_center)
+	local mat1 = Material("grass")
+	local mat2 = Material("grass32")
+	local obj = Mesh()
+	local skyobj = Mesh()
+	obj:BuildFromTriangles( verts )
+	hook.Add( "PostDrawOpaqueRenderables", "FAKK_Land", function()
+		render.SetLightingMode(2)
+		render.SetMaterial( mat1 )
+		obj:Draw()
+		render.SetMaterial( mat2 )
+		skyobj:Draw()
+		render.SetLightingMode(0)
+	end )
+	
+	local skyTerrainGenerated = false
 	function pim_holograms()
 		local entCenter = LocalPlayer()
 		if (entCenter:InVehicle()) then entCenter = entCenter:GetVehicle() end
@@ -40,20 +121,31 @@ if (!SERVER) then
 		pim_currentdim = pim_GetCurrentDim(entCenter)
 		local pim_center = pim_currentdim:GetPos()
 		local pim_location = pim_currentdim.pim_location
-		for k, v in pairs(pim_propholograms) do
-			--v:Remove()
-			v:SetPos( pim_dimtosky(pim_PosToDim(pim_proptable[k][1], pim_center, pim_location), pim_center, skyCamPos, 64))
-			v:SetAngles( pim_proptable[k][3] )
-			v:SetModel( pim_proptable[k][4] )
-			if pim_proptable[k][5] then
-				v:SetModelScale( 1/64 , 0 )
-			else
-				v:SetModelScale( 0 , 0 )
-				if k > #pim_proptable then
-					--v:Remove()
-					--table.remove( pim_propholograms, k )
+		i = 0
+		while i < #pim_propholograms do
+				i = i + 1
+				v = pim_propholograms[i]
+				--v:Remove()
+				--print(pim_proptable)
+				--print(pim_proptable[i])
+				if pim_proptable[i] == nil then
+					v:Remove()
+					table.remove( pim_propholograms, i )
+					i = i - 1
+				else
+					v:SetPos( pim_dimtosky(pim_PosToDim(pim_proptable[i][1], pim_center, pim_location), pim_center, skyCamPos, 64))
+					v:SetAngles( pim_proptable[i][3] )
+					v:SetModel( pim_proptable[i][4] )
+					if pim_proptable[i][5] then
+						v:SetModelScale( 1/64 , 0 )
+					else
+						v:SetModelScale( 0 , 0 )
+						if v > #pim_proptable then
+							--v:Remove()
+							--table.remove( pim_propholograms, v )
+						end
+					end
 				end
-			end
 		end
 		if #pim_proptable > #pim_propholograms then
 			i = #pim_propholograms
@@ -68,6 +160,16 @@ if (!SERVER) then
 				table.insert( pim_propholograms, ent )
 			end
 		end
+		skyverts = fillvert(16, (skyCamPos-pim_size)-(pim_location/64), (skyCamPos+pim_size)-(pim_location/64))
+		skyverts = skyterrainupdate(entCenter:GetPos()-pim_center, skyverts, 64, pim_location, skyCamPos)
+		skyobj:Destroy()
+		skyobj = Mesh()
+		skyobj:BuildFromTriangles( skyverts )	
+		verts = fillvert(8, pim_center-pim_size, pim_center+pim_size)
+		verts = terrainupdate(entCenter:GetPos(), verts, 1, pim_location, pim_center, (pim_size[1]*2)/8)
+		obj:Destroy()
+		obj = Mesh()
+		obj:BuildFromTriangles( verts )	
 	end
 	local entity
 	
--- ProjectInfiniteMap/lua/autorun/shared.lua
+++ ProjectInfiniteMap/lua/autorun/shared.lua
@@ -1,3 +1,11 @@
+
+pim_enabled = false
+pim_size = Vector( 7616, 7616, 7616 )
+pim_dimensiontable = {}
+pim_proptable = {}
+pim_propholograms = {}
+pim_terrainStarted = false
+
 if (!CLIENT) then
 	
 	util.AddNetworkString( "net_pim_holotable" )
@@ -45,14 +53,105 @@ if (!CLIENT) then
 		net.Broadcast()
 	end
 	
-	
 	function netReplace(dest, offset, data)
 		dest[offset] = data
 	end
+	
+	function terrainupdate (plypos, vert, mu, location, center, gridsize)
+		local pl_x = plypos[1]
+		local pl_z = plypos[2]
+		local pl_y = plypos[3]
+		local x = pim_DimToPos(vert[1].pos, center, location)[1]
+		local z = pim_DimToPos(vert[1].pos, center, location)[2]
+		local xd = x%gridsize
+		local zd = z%gridsize
+		for i = 1, #vert, 1 do
+			local xa = vert[i].pos[1]
+			local za = vert[i].pos[2]
+			xa = xa - xd
+			za = za - zd
+			vert[i].pos = Vector(xa, za, ((pperlin(location[1] + (xa-center[1]), location[2] + (za-center[2]), 4)-location[3])+center[3]) )
+			--vert[i].pos = Vector(xa, za, ((0-location[3])+center[3]) )
+		end
+		return (vert)
+	end
+	
+	function fillvert (power, vert1, vert2)
+		local vert = {}
+		local x1 = vert1[1]
+		local z1 = vert1[2]
+		local x2 = vert2[1]
+		local z2 = vert2[2]
+		local x2b = ( ( x2 - x1 ) / power ) + x1
+		local z2b = ( ( z2 - z1 ) / power ) + z1
+		local xb = math.abs(x1 - x2b)
+		local zb = math.abs(z1 - z2b)
+		local count = 1
+		for i = 1, power, 1
+		do
+			for e = 1, power, 1
+			do
+				local xb1 = x1 + (xb*(e))
+				local zb1 = z1 + (zb*(i))
+				local xb2 = x2b + (xb*(e))
+				local zb2 = z2b + (zb*(i))
+				vert[count*6-5] = { pos = Vector( xb2 , zb2 , 0) , u = 1, v = 1 }
+				vert[count*6-4] = { pos = Vector( xb2 , zb1 , 0) , u = 1, v = 0 }
+				vert[count*6-3] = { pos = Vector( xb1 , zb1 , 0) , u = 0, v = 0 }
+				vert[count*6-2] = { pos = Vector( xb1 , zb2 , 0) , u = 0, v = 1 }
+				vert[count*6-1] = { pos = Vector( xb2 , zb2 , 0) , u = 1, v = 1 }
+				vert[count*6  ] = { pos = Vector( xb1 , zb1 , 0) , u = 0, v = 0 }
+				count = count + 1
+			end
+		end
+		return (vert)
+	end
 end
 
-if (game.GetMap() == "gm_spacebox") then
-	
+local function createScatterProp(location)
+	local ent = ents.Create("prop_physics")
+	ent:SetModel("models/props_foliage/tree_springers_01a.mdl")
+	ent:SetPos(location)
+	ent:Spawn()
+	fphys = ent:GetPhysicsObject()
+	ent:SetName("TerrainScatter")
+	ent:SetSolid(SOLID_VPHYSICS)
+	fphys:EnableMotion(false)
+end
+
+local function terrainScatter(amount, center, location)
+	vert1 = center - pim_size
+	vert2 = center + pim_size
+	local i = 0
+	while i < amount do
+		local x = math.random(vert1[1], vert2[1])
+		local z = math.random(vert1[2], vert2[2])
+		local y = ((pperlin(location[1] + (x-center[1]), location[2] + (z-center[2]), 4)-location[3])+center[3]) - 40
+		createScatterProp(Vector(x,z,y))
+		i = i + 1
+	end
+end
+
+if (game.GetMap() == "gm_spaceboxsp") then
+	if (!CLIENT) then
+		hook.Add( "InitPostEntity", "FAKK", function ()
+			fakkent = ents.Create("prop_physics")
+			fakkent:SetModel("models/props_lab/cactus.mdl")
+			fakkent:SetPos(Vector( 0 , 0 , 0 ), 1)
+			fakkent:Spawn()
+			fphys = fakkent:GetPhysicsObject()
+			fphys:EnableMotion(false)
+			fakkent:SetName("TerrainCollision")
+			fakkent:SetMaterial("grass")
+			fakkent:SetColor( Color(255, 255, 255, 0 ) )
+			fakkent:DrawShadow( false )
+			fakkent:SetSolid(SOLID_VPHYSICS)
+			fakkent:EnableCustomCollisions( true )
+			fakkent:PhysicsFromMesh( verts )
+			fphys = fakkent:GetPhysicsObject()
+			fphys:EnableMotion(false)
+		end )
+	end
 	hook.Add( "Think", "Space_Test_Think", function()
 		if pim_enabled == true then
 			for kdm, dim in pairs(pim_dimensiontable) do 
@@ -64,6 +163,11 @@ if (game.GetMap() == "gm_spacebox") then
 				
 				local pim_ents1 = ents.IsSpecEntityInBox( pim_center - pim_size, pim_center + pim_size, ply )
 				local pim_ents2 = ents.IsSpecEntityInBox( pim_center - pim_size*0.25, pim_center + pim_size*0.25, ply )
+				
+				if pim_terrainStarted == false then
+					pim_terrainStarted = true
+					pimUpdate({dim, dim.pim_location})
+				end
 				
 				if (pim_ents1 == true and pim_ents2 == false ) then
 					local PosOffset = 0
@@ -80,51 +184,29 @@ if (game.GetMap() == "gm_spacebox") then
 					end
 					local pim_ents3 = ents.FindInBox( pim_center - pim_size, pim_center + pim_size )
 					for k, v in pairs(pim_ents3) do
-						if v ~= ply and v:IsPlayer() == false and v:GetClass() ~= "pim_sdc" and v:GetParent():IsValid() == false then
+						if v ~= ply and v:IsPlayer() == false and v:GetClass() ~= "pim_sdc" and v:GetParent():IsValid() == false and v:GetName() ~= "TerrainCollision" then
 							local phys = v:GetPhysicsObject()
 							if ( IsValid( phys ) ) then
 								local tele_offset = v:GetPos() - PosOffset
 								if ( tele_offset:WithinAABox( (pim_center - pim_size*0.9), (pim_center + pim_size*0.9) ) ) then
 									local vel = v:GetVelocity()
 									local phys = v:GetPhysicsObject()
-									--if phys:IsValid() then
-									--	phys:SetPos(tele_offset)
-									--end
 									v:SetPos(tele_offset)
 									phys:SetVelocity(vel)
 								else
-									pim_unload( v, dim, tele_offset, skyCamPos )
+									if v:GetName() ~= "TerrainScatter" then
+										pim_unload( v, dim, tele_offset, skyCamPos )
+									else
+										v:Remove()
+									end
 								end
 							end
 						end
 					end
-					--pim_ents3 = ents.FindInBox( skyCamPos - pim_size, skyCamPos + pim_size )
-					--for k, v in pairs(pim_ents3) do
-					--	if v:GetClass() == "prop_dynamic" then
-					--		local tele_offset = ((pim_proptable[v.pim_off][1] - dim.pim_location) / 64) + skyCamPos + (pim_center/64)
-					--		if ( tele_offset:WithinAABox( (skyCamPos - pim_size*0.9), (skyCamPos + pim_size*0.9) ) ) then
-					--			v:SetPos(tele_offset)
-					--		else
-					--			v:Remove()
-					--			print("Sky Entity Unloaded")
-					--		end
-					--		local teleconverted = tele_offset
-					--		if teleconverted:WithinAABox( pim_dimtosky((pim_center - pim_size*0.75), dim.pim_location, skyCamPos, 64), pim_dimtosky((pim_center + pim_size*0.75), dim.pim_location, skyCamPos, 64) ) then
-					--			print("Entity Reloaded")
-					--			pim_reload( v, dim, pim_center)
-					--		end
-					--	end
-					--end
 					local i = 1
 					while #pim_proptable >= i do
 						v = pim_proptable[i]
 						local tele_offset = ((v[1] - dim.pim_location) / 64) + skyCamPos + (pim_center/64)
-						--if ( tele_offset:WithinAABox( (skyCamPos - pim_size*0.9), (skyCamPos + pim_size*0.9) ) ) then
-						--	v:SetPos(tele_offset)
-						--else
-						--	v:Remove()
-						--	print("Sky Entity Unloaded")
-						--end
 						local tele_offset2 = v[1]
 						if tele_offset2:WithinAABox( pim_DimToPos(pim_center - pim_size*0.75, pim_center, dim.pim_location), pim_DimToPos(pim_center + pim_size*0.75, pim_center, dim.pim_location) ) then
 							pim_reload( v, dim, pim_center)
@@ -149,28 +231,20 @@ if (game.GetMap() == "gm_spacebox") then
 							end
 						end
 					end
-					local pim_ents1 ents.FindInBox( pim_center - pim_size, pim_center + pim_size )
-					local pim_ents2 ents.FindInBox( pim_center - pim_size*0.75, pim_center + pim_size*0.75 )
-					--for k, v in pairs(pim_ents1) do
-					--	if not v in pim_ents2 then
-					--		if v:IsPlayer() or v:GetClass() = "pim_persistent" then
-					--			print("Forked")
-					--			pim_CREATEDIMENSION( v, nil, (v:GetPos-pim_center) + dim.pim_location, Vector(0, 0, 0) )
-					--			local persists = ents.FindPlayersInBox( pim_center - pim_size, pim_center + pim_size )
-					--			if persists == nil then
-					--				dim.pim_pair = nil
-					--			else
-					--				dim.pim_pair = persists[1]
-					--				if #persists > 1 then
-					--					for key, ent in pairs(persists) do
-					--						pim_CREATEDIMENSION( ent, nil, (ent:GetPos-pim_center) + dim.pim_location, Vector(0, 0, 0) )
-					--					end
-					--				end
-					--			end
-					--		end
-					--	end
-					--end
 					pimUpdate({dim, dim.pim_location})
+					local i = 0
+					for k, v in pairs(pim_ents3) do
+						if v:GetName() == "TerrainScatter" then
+							i = i + 1
+						end
+					end
+					i = 4 - i
+					if i > 0 then terrainScatter(i, pim_center, dim.pim_location) end
+					verts = fillvert(8, pim_center-pim_size, pim_center+pim_size)
+					verts = terrainupdate(ply:GetPos(), verts, 1, dim.pim_location, pim_center, (pim_size[1]*2)/8)
+					fakkent:PhysicsFromMesh( verts )
+					fphys = fakkent:GetPhysicsObject()
+					fphys:EnableMotion(false)
 				end
 			end
 		end
